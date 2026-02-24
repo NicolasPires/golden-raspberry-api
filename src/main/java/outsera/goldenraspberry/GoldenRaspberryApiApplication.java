30fec47821c0ed289f4a1e1fff4f3103350b886e
Nicolas Pires <nicolask.pires@gmail.com>
2026-02-24 15:22:39 -0300

    chore: bootstrap project with Spring Initializr

---
+++ src/main/java/outsera/goldenraspberry/GoldenRaspberryApiApplication.java
@@ -0,0 +1,13 @@
+package outsera.golden_raspberry_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenRaspberryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenRaspberryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/outsera/goldenraspberry/GoldenRaspberryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package outsera.golden_raspberry_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
